5bfcd2b6b1c78ee5a79f3036e51527f06f4bc71b
aldinkapetanovic@gmail.com <aldinkapetanovic@gmail.com>
2022-12-13 22:29:50 +0100

    web, api, db, compose

--- server/app.js
+++ server/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 app.use(cors());
 
+mongoose.connect('mongodb://db:27017/mydb', { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  years: Number
+});
+
+const User = mongoose.model('users', userSchema);
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      res.json(users);
+    }
+  });
 });
 
 app.listen(8080, () => {
