6767a895847428e470684a4842490860210fdffd
aldinkapetanovic@gmail.com <aldinkapetanovic@gmail.com>
2022-12-14 10:38:58 +0100

    README

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [result, setResult] = useState(null);
+  const [hello, setHello] = useState(null);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8080')
+    fetch("http://localhost:8080")
       .then(response => response.json())
-      .then(data => setResult(data));
+      .then(data => setHello(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then(response => response.json())
+      .then(data => setUsers(data));
   }, []);
 
   return (
     <div>
-      {result && <h1>{result.message}</h1>}
+      {hello && <h1>{hello.message}</h1>}
+      {users &&
+        users.map((user, index) => <p key={index}>{JSON.stringify(user)}</p>)}
     </div>
   );
 }
-
 
 export default App;
